89084bbb8547fe58eddc52d3b21ac4e0bba06699
unknown <Alex@.(none)>
2010-07-23 20:31:42 +0200

    changed s toc

--- js/typeText.js
+++ js/typeText.js
@@ -32,7 +32,7 @@ $(document).ready(function() {
 								typingText("oooo\n\`888\n 888   .ooooo.   .ooooo.  ooo. .oo.    .ooooo.  oooo    ooo      .oooo.o  .ooooo. \n 888  d88\' \`88b d88\' \`88b \`888P\"Y88b  d88\' \`88b  \`88.  .8\'      d88\(  \"8 d88\' \`88b\n 888  888ooo888 888   888  888   888  888   888   \`88..8\'       \`\"Y88b.  888ooo888\n 888  888    .o 888   888  888   888  888   888    \`888\'   .o.  o.  \)88b 888    .o\no888o \`Y8bod8P\' \`Y8bod8P\' o888o o888o \`Y8bod8P\'     \`8\'    Y8P  8\"\"888P\' \`Y8bod8P\'","#box4",false);
 								setTimeout(function() {
 									$("#green3").show();
-									typingText("Welcome to my syberworld! \=\)","#box5",true);
+									typingText("Welcome to my cyberworld! \=\)","#box5",true);
 									setTimeout(function() {
 										$(".image").fadeIn(2000);
 //										setTimeout(function() {
