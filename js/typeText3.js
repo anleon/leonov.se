d3b51f952b2e531ca6e32e3b773705c7f699bdba
anleon <alexander@leonov.se>
2010-12-01 23:09:20 +0100

    Blinking kursor

---
+++ js/typeText3.js
@@ -0,0 +1,28 @@
+/*
+ * 
+ */
+
+$(document).ready(function() {
+	var jsConveyor
+	function typingText(text,boxNumber,delayOn) {
+		if (i < text.length) {
+			if(delayOn == true){
+				delay = Math.floor(Math.random() * 200);
+			}else{
+				delay = 0;
+			}
+			//console.log(delayOn);
+			$(boxNumber).append(text.charAt(i));
+			i++;
+			setTimeout(function() {
+				typingText(text,boxNumber,delayOn);
+			}, delay);
+		} else {
+			i = 0;
+		}
+	}
+	
+	function typedText(tag,text) {
+		$(tag).html(text);
+	}
+});
