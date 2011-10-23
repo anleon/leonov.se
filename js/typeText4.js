4b3a8a16cada1209a1026d9e16aa0978908c55db
alexander <alexander@leonov.se>
2011-10-23 23:31:33 +0200

    leonov.se v2.0

--- js/typeText4.js
+++ js/typeText4.js
@@ -1,6 +1,7 @@
 /*
  * 
  */
+
 $(document).ready(function() {
 	var text = "";
 	var boxNumber = "";
@@ -18,6 +19,8 @@ $(document).ready(function() {
 	var endTime = 0;
 	var totalDelay = 0;
 	var timeDifference = 0;
+	var timeOutId = "#";
+	var empty = false;
 	var conveyorBand = new Array();
 	conveyorBand[0] = ['tag','#green1',true,1000];
 	conveyorBand[1] = ['typedText','#box','logged as: ',1000];
@@ -27,7 +30,7 @@ $(document).ready(function() {
 	conveyorBand[5] = new Array('typedText','#box2','alexander@leonov.se\'s password: ',1000);
 	conveyorBand[6] = new Array('typingText','#box3','*********',true,1500);
 	conveyorBand[7] = new Array('tag','#green2',false,500);
-	conveyorBand[8] = new Array("typingText","#box4 pre","oooo\n\`888\n 888   .ooooo.   .ooooo.  ooo. .oo.    .ooooo.  oooo    ooo      .oooo.o  .ooooo. \n 888  d88\' \`88b d88\' \`88b \`888P\"Y88b  d88\' \`88b  \`88.  .8\'      d88\(  \"8 d88\' \`88b\n 888  888ooo888 888   888  888   888  888   888   \`88..8\'       \`\"Y88b.  888ooo888\n 888  888    .o 888   888  888   888  888   888    \`888\'   .o.  o.  \)88b 888    .o\no888o \`Y8bod8P\' \`Y8bod8P\' o888o o888o \`Y8bod8P\'     \`8\'    Y8P  8\"\"888P\' \`Y8bod8P\'",false,1300);
+	conveyorBand[8] = new Array("typingText","#box4 pre","oooo\n\`888\n 888   .ooooo.   .ooooo.  ooo. .oo.    .ooooo.  oooo    ooo      .oooo.o  .ooooo. \n 888  d88\' \`88b d88\' \`88b \`888P\"Y88b  d88\' \`88b  \`88.  .8\'      d88\(  \"8 d88\' \`88b\n 888  888ooo888 888   888  888   888  888   888   \`88..8\'       \`\"Y88b.  888ooo888\n 888  888    .o 888   888  888   888  888   888    \`888\'   .o.  o.  \)88b 888    .o\no888o \`Y8bod8P\' \`Y8bod8P\' o888o o888o \`Y8bod8P\'     \`8\'    Y8P  8\"\"888P\' \`Y8bod8P\'",false,5300);
 	conveyorBand[9] = new Array('tag','#green3',true,1000);
 	conveyorBand[10] = new Array('typingText','#box5','Welcome to my cyberworld!',true,0);
 	
@@ -42,11 +45,16 @@ $(document).ready(function() {
 	}
 	
 	function conveyor(conveyorBand,n) {
+//		console.log(conveyorBand.length);
 		if(n < conveyorBand.length){
 			if(conveyorBand[n][0] == "tag"){
+				
+//				console.log(conveyorBand[n][0]);
+				
 				if(conveyorBand[n][2] == true){
 					$(conveyorBand[n][1]).show();
 				}else{
+					
 					$(conveyorBand[n][1]).hide();
 				}
 				setTimeout(function(){
@@ -54,35 +62,38 @@ $(document).ready(function() {
 					conveyor(conveyorBand,n);
 				},conveyorBand[n][3]);
 			}else if(conveyorBand[n][0] == "typedText"){
+//				console.log(conveyorBand[n][0]);
 				typedText(conveyorBand[n][1],conveyorBand[n][2]);
 				setTimeout(function(){
 					n++;
+					
 					conveyor(conveyorBand,n);
 				},conveyorBand[n][3]);
 			}else if(conveyorBand[n][0] == "typingText"){
-				if (i < conveyorBand[n][2].length) {
-					if(conveyorBand[n][3] == true){
-						delay = Math.floor(Math.random() * 200);
-						totalDelay += delay;
-					}else{
-						delay = 0;
-					}
-					$(conveyorBand[n][1]).append(conveyorBand[n][2].charAt(i));
-					i++;
-					setTimeout(function() {
-						conveyor(conveyorBand,n);
-					}, delay);
-				} else {
-					i = 0;
-					setTimeout(function(){
+				if(j == 0){
+//					var startTime = 0;
+					startTime = new Date();
+					j++;
+				}
+				typingText(conveyorBand[n][1],conveyorBand[n][2],conveyorBand[n][3]);
+				
+				if (timeOutId == 0){
+					empty = false;
+					timeOutId = setTimeout(function(){
 						n++;
 						conveyor(conveyorBand,n);
 					},conveyorBand[n][4]);
+				} else{
+					empty = true;
+					setTimeout(function(){
+						conveyor(conveyorBand,n);
+					},500);
 				}
 			}
 		}else{
 			setTimeout(function(){
 				if(height >= 440){
+//					console.log($(".image #text_foto").height());
 					imgHeight = height - 255;
 					imgWidth = imgHeight * (494/668);
 					$(".image, .image #text_foto").height(imgHeight).width(imgWidth);
@@ -91,6 +102,43 @@ $(document).ready(function() {
 				$(".div_links").hide().css("visibility", "visible").fadeIn("slow");
 				blinker("#green3");
 			}, 3000);
+		}
+	}
+	
+	function typingText(boxNumber,text,delayOn) {
+		if (!empty) {
+			if (i < text.length) {
+				if(delayOn == true){
+					delay = Math.floor(Math.random() * 200);
+					totalDelay += delay;
+	//				console.log(totalDelay);
+				}else{
+					delay = 0;
+					
+				}
+				
+				//console.log(delayOn);
+				$(boxNumber).append(text.charAt(i));
+				i++;
+				
+				setTimeout(function() {
+					typingText(boxNumber,text,delayOn);
+				}, delay);
+			} else {
+				timeOutId = 0;
+				if(j == 1 && i == text.length){
+	//				var endTime = 0;
+					endTime = new Date();
+					j++;
+					console.log("totalDelay: "+totalDelay);
+					console.log("endTime.getTime(): "+endTime.getTime());
+					console.log("startTime.getTime(): "+startTime.getTime());
+					console.log((endTime.getTime()-startTime.getTime()));
+					console.log("(endTime.getTime()-startTime.getTime())-totalDelay: "+(((endTime.getTime()-startTime.getTime())-totalDelay)));
+					console.log("(((endTime.getTime()-startTime.getTime())-totalDelay))*465: "+(((endTime.getTime()-startTime.getTime())-totalDelay))*465);
+				}
+				i = 0;
+			}
 		}
 	}
 	
@@ -107,5 +155,6 @@ $(document).ready(function() {
 			}, 1000);
 		}, 1000);
 	}
+	
 	conveyor(conveyorBand,n);
 });
